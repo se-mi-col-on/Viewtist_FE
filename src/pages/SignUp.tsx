9c4f74562a596d1190a1ded1e5f21b672cfa95e6
DxxxHxx <donghun1163@gmail.com>
2024-03-02 17:01:51 +0900

    Design:채널설정페이지,회원가입 폼 변경

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -29,67 +29,65 @@ export default function SignUp() {
 
   //<div className='flex items-center w-full mt-6 border-2'>
   return (
-    <div>
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className='flex flex-col items-center justify-center border-2'
-      >
-        <h1 className='text-3xl'>회원가입</h1>
-        <label className='w-full max-w-xs form-control'>
-          <div className='label'>
-            <span className='font-bold label-text'>아이디</span>
-          </div>
+    <div className='p-5 '>
+      <form className='max-w-md mx-auto' onSubmit={handleSubmit(onSubmit)}>
+        <h1 className='text-center sm:text-xl md:text-3xl mb-7'>회원가입</h1>
+        <div className='relative z-0 w-full mb-5 group'>
           <input
             type='text'
-            placeholder='Type here'
-            className={`input input-bordered  ${
-              (errors.id && 'border-red-700',
-              errors.id ? 'focus:border-red-700' : 'focus:border-green-500')
-            }`}
+            id='floating_id'
+            className='block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer'
+            placeholder=' '
             {...register('id', {
-              required: '아이디를 입력해주세요.',
+              required: '필수 항목입니다.',
               pattern: {
                 value: /^[a-z]+[a-z0-9]{5,19}$/g,
                 message: '영문자로 시작, 영문자 + 숫자 6~20자 ',
               },
             })}
           />
-          {errors.id && <span className='mt-3 text-red-700'>{errors.id.message}</span>}
-        </label>
-
-        <label className='w-full max-w-xs form-control'>
-          <div className='label'>
-            <span className='font-bold label-text'>비밀번호</span>
-          </div>
+          <label
+            htmlFor='floating_id'
+            className='peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'
+          >
+            아이디
+          </label>
+          <button
+            type='button'
+            className='absolute top-0 right-0 btn btn-outline sm:btn-xs md:btn-sm'
+          >
+            중복검사
+          </button>
+          {errors.id && <span className='text-red-700 '>{errors.id.message}</span>}
+        </div>
+        <div className='relative z-0 w-full mb-5 group'>
           <input
             type='password'
-            placeholder='Type here'
-            className={`input input-bordered  ${
-              (errors.pw && 'border-red-700',
-              errors.pw ? 'focus:border-red-700' : 'focus:border-green-500')
-            }`}
+            id='floating_password'
+            className='block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer'
+            placeholder=' '
             {...register('pw', {
-              required: '비밀번호를 입력해주세요.',
+              required: '필수 항목입니다.',
               pattern: {
                 value: /^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z]{8,16}$/,
                 message: '8 ~ 16자 영문, 숫자 조합',
               },
             })}
           />
-          {errors.pw && <span className='mt-3 text-red-700'>{errors.pw.message}</span>}
-        </label>
-
-        <label className='w-full max-w-xs form-control'>
-          <div className='label'>
-            <span className='font-bold label-text'>비밀번호 확인</span>
-          </div>
+          <label
+            htmlFor='floating_password'
+            className='peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'
+          >
+            비밀번호
+          </label>
+          {errors.pw && <span className='text-red-700 '>{errors.pw.message}</span>}
+        </div>
+        <div className='relative z-0 w-full mb-5 group'>
           <input
             type='password'
-            placeholder='Type here'
-            className={`input input-bordered  ${
-              (errors.pwRequired && 'border-red-700',
-              errors.pwRequired ? 'focus:border-red-700' : 'focus:border-green-500')
-            }`}
+            id='floating_repeat_password'
+            className='block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer'
+            placeholder=' '
             {...register('pwRequired', {
               required: '비밀번호를 다시 입력해주세요.',
               minLength: { message: '최소 5자 이상 입력해주세요.', value: 5 },
@@ -103,22 +101,21 @@ export default function SignUp() {
               },
             })}
           />
-          {errors.pwRequired && (
-            <span className='mt-3 text-red-700'>{errors.pwRequired.message}</span>
-          )}
-        </label>
+          <label
+            htmlFor='floating_repeat_password'
+            className='peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'
+          >
+            비밀번호 확인
+          </label>
+          {errors.pwRequired && <span className='text-red-700 '>{errors.pwRequired.message}</span>}
+        </div>
 
-        <label className='w-full max-w-xs form-control'>
-          <div className='label'>
-            <span className='font-bold label-text'>이메일</span>
-          </div>
+        <div className='relative z-0 w-full mb-5 group'>
           <input
             type='email'
-            placeholder='Type here'
-            className={`input input-bordered  ${
-              (errors.email && 'border-red-700',
-              errors.email ? 'focus:border-red-700' : 'focus:border-green-500')
-            }`}
+            id='floating_repeat_email'
+            className='block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer'
+            placeholder=' '
             {...register('email', {
               pattern: {
                 value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
@@ -127,25 +124,45 @@ export default function SignUp() {
               required: '필수 항목입니다.',
             })}
           />
-          {errors.email && <span className='mt-3 text-red-700'>{errors.email.message}</span>}
-        </label>
+          <label
+            htmlFor='floating_repeat_email'
+            className='peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'
+          >
+            이메일
+          </label>
+          <button
+            type='button'
+            className='absolute top-0 right-0 btn btn-outline sm:btn-xs md:btn-sm'
+          >
+            이메일 인증
+          </button>
+          {errors.email && <span className='text-red-700 '>{errors.email.message}</span>}
+        </div>
 
-        <label className='w-full max-w-xs form-control'>
-          <div className='label'>
-            <span className='font-bold label-text'>닉네임</span>
-          </div>
+        <div className='relative z-0 w-full mb-5 group'>
           <input
             type='text'
-            placeholder='Type here'
-            className={`input input-bordered  ${
-              (errors.nickName && 'border-red-700',
-              errors.nickName ? 'focus:border-red-700' : 'focus:border-green-500')
-            }`}
-            {...register('nickName')}
+            id='floating_repeat_nickname'
+            className='block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer'
+            placeholder=' '
+            {...register('nickName', {
+              pattern: {
+                value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                message: '2자 이상 16자 이하, 영어 또는 숫자 또는 한글로 구성',
+              },
+              required: '필수 항목입니다.',
+            })}
           />
-        </label>
+          <label
+            htmlFor='floating_repeat_nickname'
+            className='peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'
+          >
+            닉네임
+          </label>
+          {errors.nickName && <span className='text-red-700 '>{errors.nickName.message}</span>}
+        </div>
 
-        <button className='mt-5 text-white btn btn-success'>회원가입 완료</button>
+        <button className='sm:btn-sm md:btn-md btn btn-outline'>회원가입</button>
       </form>
     </div>
   );
