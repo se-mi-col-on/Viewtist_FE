fbcf4b8b971b25a4c4bc6a4189c2eadc1efb79f3
DxxxHxx <donghun1163@gmail.com>
2024-03-15 15:18:00 +0900

    Feat:액세스 토큰 재발급

---
+++ src/utils/signIn/useRefreshToken.ts
@@ -0,0 +1,30 @@
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+
+export const useRefreshToken = (accessToken: string, refreshToken: string) => {
+  useQuery({
+    queryKey: ['refreshToken'],
+    queryFn: async () => {
+      try {
+        const res = await axios
+          .post(
+            `/api/api/users/refresh-token`,
+            {
+              refreshToken,
+            },
+            {
+              headers: {
+                Authorization: `Bearer ${accessToken}`,
+                Accept: '*/*',
+              },
+            },
+          )
+          .then((res) => res.data);
+        console.log(res);
+        return res;
+      } catch (e) {
+        console.log(e);
+      }
+    },
+  });
+};
