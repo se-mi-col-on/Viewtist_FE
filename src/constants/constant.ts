0a58facdcc21aded5dd8f6e3826bf34f79e38ba4
JA_X <envijs93@gmail.com>
2024-03-18 03:38:12 +0900

    Fix: 카테고리 영문화

--- src/constants/constant.ts
+++ src/constants/constant.ts
@@ -1,2 +1,2 @@
-export const STREAM_CATEGORY_LIST: string[] = ['음악', '댄스', '그림', '사진', '연기'];
+export const STREAM_CATEGORY_LIST: string[] = ['MUSIC', 'DANCE', 'DRAW', 'PICTURE', 'ACT'];
 export const STREAM_URL: string = 'rtmp://15.164.226.60:1935/live';
