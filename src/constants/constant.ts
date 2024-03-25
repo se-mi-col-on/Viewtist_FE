26e3266289fe910800361edb43ed55018ffd8ab3
DxxxHxx <donghun1163@gmail.com>
2024-03-25 15:02:38 +0900

    Feat:소셜 로그인 초안

--- src/constants/constant.ts
+++ src/constants/constant.ts
@@ -1,3 +1,4 @@
 export const STREAM_CATEGORY_LIST: string[] = ['MUSIC', 'DANCE', 'DRAW', 'PICTURE', 'ACT'];
 export const STREAM_URL: string = 'rtmp://15.164.226.60:1935/live';
-export const CHAT_END_POINT = `https://ec2-3-35-234-97.ap-northeast-2.compute.amazonaws.com/live/chat`;
+export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${import.meta.env.VITE_REST_API_KEY}&redirect_uri=${import.meta.env.VITE_REDIRECT_URI}&response_type=code`;
+export const KAKAO_IMG_URL = `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMpuyXaaKG7gnNP-b1POYbT9kSiqG0bGXVfA&usqp=CAU`;
