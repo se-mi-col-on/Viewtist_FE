8d4bb4cb9bf75c34f3568882a21cf503bc35f123
DxxxHxx <donghun1163@gmail.com>
2024-03-26 15:05:02 +0900

    Style:devtool 삭제

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { RecoilRoot } from 'recoil';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import App from './App.tsx';
 
 const queryClient = new QueryClient();
@@ -14,6 +13,5 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <RecoilRoot>
       <App />
     </RecoilRoot>
-    <ReactQueryDevtools initialIsOpen={false} />
   </QueryClientProvider>,
 );
