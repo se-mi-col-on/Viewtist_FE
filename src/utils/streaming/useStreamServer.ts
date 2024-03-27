de79e1898dc7f5df93df1cbc406f54f3a272ae06
JA_X <envijs93@gmail.com>
2024-03-28 00:42:38 +0900

    Fix: https 통신을 위한 streamServer URL 변경

--- src/utils/streaming/useStreamServer.ts
+++ src/utils/streaming/useStreamServer.ts
@@ -2,5 +2,6 @@ import { useStreamKey } from './useStreamKey';
 
 export const useStreamServer = (): string => {
   const { data: streamKey } = useStreamKey();
-  return `https://15.164.226.60:8080/hls/${streamKey}_src.m3u8`;
+  // return `https://15.164.226.60:8080/hls/${streamKey}_src.m3u8`;
+  return `https://ec2-15-164-226-60.ap-northeast-2.compute.amazonaws.com/hls/${streamKey}_src.m3u8`;
 };
